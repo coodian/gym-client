25fe2b40fbf41bc9fc326ad9da53a73d2724f5aa
hasanrana21 <hasanrana178821@gmail.com>
2022-01-03 15:50:53 +0600

    testing commit from test branch

--- src/components/home-page/Home.js
+++ src/components/home-page/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>This in our Brand new GYM Project</h1>
+      <h1>This in our Brand new GYM Projectssss</h1>
     </div>
   );
 };
