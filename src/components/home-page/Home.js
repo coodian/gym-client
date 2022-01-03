08a60319d27b7e4fbb90774d630ce46fb1a19a81
hasanrana21 <hasanrana178821@gmail.com>
2022-01-03 16:00:19 +0600

    review testing bugs fix

--- src/components/home-page/Home.js
+++ src/components/home-page/Home.js
@@ -3,7 +3,10 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1>This in our Brand new GYM Projectssss || We Are testing</h1>
+      <h1>
+        This in our Brand new GYM Projectssss || We Are testing || Review bugs
+        completed
+      </h1>
     </div>
   );
 };
